4d0e381dc28d42a64ab1a9c9989582b849c53f8b
mikkel node <mikkel@node>
2025-11-28 14:06:42 +0000

    laget en models folder med puppy.js med et basic schema

---
+++ models/puppy.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const puppySchema = new mongoose.Schema({
+    name: String,
+    puppyPower: Number,
+    bestFriend: String,
+    birthYear: Number,
+    imageUrl: String,
+
+});
+
+module.exports = mongoose.model('Puppy', puppySchema);
