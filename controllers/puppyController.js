bc0721ca5b00a2e4b0a0fb471b79c9d501ecb0b2
mikkel node <mikkel@node>
2025-11-28 14:18:20 +0000

    laget en puppycontroller.js med fungerende js

---
+++ controllers/puppyController.js
@@ -0,0 +1,12 @@
+const Puppy = require('../models/puppy');
+
+exports.getPuppy = async (req, res)=> {
+    const name = req.params.name;
+    const puppy = await Puppy.findOne({name});
+
+    if(!Puppy) {
+        return res.status(404).send("Puppy is gone");
+    }
+
+    res.render('puppy', {puppy});
+};
