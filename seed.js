a3681fac11f05fe255993d67584255676b014990
mikkel node <mikkel@node>
2025-12-01 12:53:46 +0000

    laget en fungerende seed for generation av puppies

--- seed.js
+++ seed.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+const Puppy = require('./models/Puppy');
 
-const puppySchema = new mongoose.Schema({
-    name: String,
-    puppyPower: Number,
-    bestFriend: String,
-    birthYear: Number,
-    imageUrl: String
-});
+mongoose.connect('mongodb://10.12.7.102:27017/rainbow-puppies')
 
-const Puppy = mongoose.model('Puppy', puppySchema);
+.then(async () => {
+  console.log("Connected to MongoDB!");
 
+  const puppies = [];
+
+  for (let i = 1; i <= 10; i++) {
+    puppies.push({
+      name: `Puppy${i}`,
+      puppyPower: Math.floor(Math.random() * 100),
+      bestFriend: `Friend${i}`,
+      birthYear: 2020 + i,
+      imageUrl: `http://10.12.7.103/img/puppy/puppy${i}.png`
+    });
+  }
+
+  await Puppy.insertMany(puppies);
+  console.log("Inserted 10 puppies!");
+  mongoose.disconnect();
+})
+.catch(err => console.error(err));
