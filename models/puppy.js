cbc0c09c96d95dd851d17a4b6f1b53c75eb88834
mikkel node <mikkel@node>
2025-12-01 12:30:37 +0000

    lagt til 'collection: rainbow-puppies' sånn at mongodb kobler til riktig db

--- models/puppy.js
+++ models/puppy.js
@@ -6,6 +6,6 @@ const puppySchema = new mongoose.Schema({
     bestFriend: String,
     birthYear: Number,
     imageUrl: String
-});
+}, { collection: "rainbow-puppies"});
 
 module.exports = mongoose.model('Puppy', puppySchema);
