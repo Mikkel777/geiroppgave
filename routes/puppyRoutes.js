f13950edc843b9f942761b0aec7f1c770be99f15
mikkel node <mikkel@node>
2025-12-05 11:47:16 +0000

    lagt til en bra routes.js

--- routes/puppyRoutes.js
+++ routes/puppyRoutes.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const puppyController = require('./controllers/puppyController');
+const Puppy = require('./models/Puppy');
 
-router.get('/puppy/:name', puppyController.getPuppy);
+router.get('/puppies', async(req, res)=> {
+    try {
+        const puppies = await Puppy.find();
+        res.json(puppies);
+    } catch (err) {
+        res.status(500).json({error:err.message});
+    }
+});
+
+router.get('/puppies/:name', async (req, res)=> {
+    try {
+        const name = req.params.name;
+        const puppy = await Puppy.findOne({name});
+
+        if (!puppy) {
+            return res.status(404).json({error: "puppy gone"});
+        }
+        res.json(Puppy);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+});
+
+module.exports = router;
