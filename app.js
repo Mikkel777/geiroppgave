9eed8054a46e314dae1958c785d316290e979bf0
mikkel node <mikkel@node>
2025-12-05 12:01:18 +0000

    oppdatert app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const puppyRoutes = require('./routes/puppyRoutes');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
 
 mongoose.connect("mongodb://10.12.7.102:27017/rainbow-puppies")
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error(err));
 
-app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
+app.use('/api', puppyRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Rainbowpuppies hjemmeside :=)");
+app.use('/img', express.static('/var/www/rainbow-puppies/img'));
+
+app.get('/', (req, res) => {
+    res.send('');
 });
-
-app.get("/puppy/:navn", puppyController.getPuppy);
-
-app.use(express.static('public'));
-
 
 app.listen(3000, ()=> {
     console.log("Serveren er online på localhost:3000!");
