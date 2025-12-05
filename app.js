bde163e89fa91cd000197b2611770190afb004e5
mikkel node <mikkel@node>
2025-12-05 13:16:34 +0000

    lagt til en static for public

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 
 app.use('/api', puppyRoutes);
