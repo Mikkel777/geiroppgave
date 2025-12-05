8f4bcfacaa2b192ce1413ba4fe2004807408a1d6
mikkel node <mikkel@node>
2025-12-05 12:28:46 +0000

    sm¨å endring i puppyroutes

--- routes/puppyRoutes.js
+++ routes/puppyRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Puppy = require('../controllers/puppyController');
+const puppyController = require('../controllers/puppyController');
+
 
 router.get('/puppies', puppyController.getEveryPuppy);
+
 router.get('/puppy/:name', puppyController.getPuppyName);
 
 module.exports = router;
