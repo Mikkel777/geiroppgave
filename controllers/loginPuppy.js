89d4800d0a3a90dae1fb727a47f02b0c0228b041
mikkel node <mikkel@node>
2025-12-08 11:17:00 +0000

    lagt til mere kode til controllers, vurderer og bruke geir sin kode istedet.

--- controllers/loginPuppy.js
+++ controllers/loginPuppy.js
@@ -7,4 +7,21 @@ const visLogin = async (req, res) => {
 const doLogin = async (req, res) => {
     const {username, password} = req.body;
     console.log("Prøver å logge inn for:", username);
-}
+    const user = await User.findOne({username});
+    if (!user) {
+        return res.render('login', {error: "Valpbruker ble ikke funnet"});
+    }
+    if(user.password !== password) {
+        return res.render('login', {error: "Feil passord"});
+    }
+    req.session.user = {username};
+    res.redirect('/profile');
+};
+
+const logout = (req, res) => {
+    req.session.destroy(()=> {
+        res.redirect('/login');
+    });
+};
+
+module.exports = {visLogin, doLogin, logout};
