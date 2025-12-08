02faea64e42456d82d36ab0f8bbfff31af64621a
mikkel node <mikkel@node>
2025-12-08 10:14:41 +0000

    lagt til vislogin og doLogin i loginpuppy

--- controllers/loginPuppy.js
+++ controllers/loginPuppy.js
@@ -1,21 +1,10 @@
 const User = require('../models/user');
 
-//AUTH
-const autentiser = async (req, res, next) =>{
-    console.info("Autentiserer bruker...");
-    const user = req.session.user;
-    if(!user) {
-        console.warn("Autentisering feilet. Sender til Login page");
-        res.redirect('/login');
-        return;
-    }
-
-//USER
-    const brukerFinnes = await user.findOne({username: user.username});
-    if (!brukerFinnes) {
-        console.warn('Valpebruker ble ikke funnet i databasen. Sender til Login Page');
-        res.session.destroy();
-        res.redirect('/login');
-        return;
-    }
+const visLogin = async (req, res) => {
+    res.render('login', { error:null });
 };
+
+const doLogin = async (req, res) => {
+    const {username, password} = req.body;
+    console.log("Prøver å logge inn for:", username);
+}
