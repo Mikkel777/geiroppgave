25a98133902e1c28595ce7aac2d89d8a91960b38
mikkel node <mikkel@node>
2025-12-12 10:23:45 +0000

    tror auth vurde være koblet til

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-
 const {
     getlogin,
     postlogin,
     logout,
     profile
 } = require ('../controllers/loginPuppy');
+
+const auth = require('../middleware/auth');
 
 router.get('/login', getlogin);
 router.post('/login', postlogin);
