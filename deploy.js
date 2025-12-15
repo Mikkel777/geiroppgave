4680d07cd6514b20ae44f103ffdf6a4965df3f5b
mikkel node <mikkel@node>
2025-12-15 14:27:31 +0000

    lagt til geir sin deploy

---
+++ deploy.js
@@ -0,0 +1,17 @@
+const dbHandler = require('./handler/handler');
+const User = require('./models/user');
+
+const users = [
+  { username: 'testuser', password: 'password123' },
+  { username: 'johndoe', password: 'qwerty' }
+];
+
+dbHandler.connectDB().then(() => {
+    console.info('Seeding database with initial users...');
+    users.forEach(u => {
+        const user = new User(u);
+        user.save()
+            .then(() => console.info(`User ${u.username} saved to database.`))
+            .catch(err => console.error(`Error saving user ${u.username}:`, err));
+    });
+});
