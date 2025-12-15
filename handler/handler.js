530f85ac5c386a387ea3138a75a404702ef63595
mikkel node <mikkel@node>
2025-12-15 13:26:06 +0000

    lagt til handler for main branch

---
+++ handler/handler.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://10.12.7.102:27017/rainbow-puppies");
+        console.log("Mongodb tilkoblet!");
+    } catch (err) {
+        console.error("Error med mongodb kobling", err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
