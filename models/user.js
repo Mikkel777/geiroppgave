8abe90ab02ee8afa345e7320d045a2189555aac1
mikkel node <mikkel@node>
2025-12-15 14:11:44 +0000

    oppdatert models user for hashing av passord

--- models/user.js
+++ models/user.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
